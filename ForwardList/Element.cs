8abf5399bcb1cb121f84d366241617fb64c122d9
Lapin_qh81@top-academy.ru <Lapin_qh81@top-academy.ru>
2024-09-15 20:25:27 +0900

    Add project files.

---
+++ ForwardList/Element.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForwardList
+{
+	internal class Element
+	{
+		public int Data { get; set; }		//Значение элемента
+		public Element pNext { get; set; }	//Следующий элемент
+
+		public Element (int Data, Element pNext)
+		{
+			this.Data = Data;
+			this.pNext = pNext;
+            Console.WriteLine($"EConstr:\t{GetHashCode()}");
+		}
+
+		~Element()
+		{
+            Console.WriteLine($"EDestr:\t{GetHashCode()}");
+		}
+	}
+}
