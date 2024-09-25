dd4477ff179e5ec8ef4200bff82c055dfe77d618
D4STEW143 <Lap1nD1ma1997>
2024-09-25 18:10:29 +0900

    Functions done.

--- List2/main.cpp
+++ List2/main.cpp
@@ -36,7 +36,6 @@ public:
 		size = 0;
 		cout << "LConstr: \t" << this << endl;
 	}
-
 	~List()
 	{
 		cout << "LDestr: \t" << this << endl;
@@ -62,6 +61,68 @@ public:
 			Head = New;
 		}
 		size++;
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+			Head = Tail = new Element(Data);
+		else
+		{
+			Element* New = new Element(Data);
+			New->pPrev = Tail;
+			Tail->pNext = New;
+			Tail = New;
+		}
+		size++;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index > size) return;
+		if (Index == size) { push_back(Data); return; }
+		if (Index == 0) { push_front(Data); return; }
+		else
+		{
+			Element* Temp = Head;
+			for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+			Temp->pNext = new Element(Data, Temp->pNext, Temp->pPrev);
+			size++;
+		}
+	}
+
+//Deleting elements
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr) return;
+		else
+		{
+			Head = Head->pNext;
+			Head->pPrev = nullptr;
+		}
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr && Tail == nullptr) return;
+		else
+		{
+			Tail = Tail->pPrev;
+			Tail->pNext = nullptr;
+		}
+		size--;
+	}
+	void erase(int Index)
+	{
+		if (Index > size) return;
+		if (Index == size) pop_back(); 
+		if (Index == 0) pop_front();
+		else
+		{
+			Element* Temp = Head;
+			for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+			Temp->pNext = Temp->pNext->pNext;
+			Temp->pPrev->pPrev = Temp->pPrev;
+			size--;
+		}
 	}
 
 	//All
@@ -90,12 +151,40 @@ void main()
 	int n;
 	cout << "Введите размер списка: "; cin >> n;
 	List list;
+	cout << "Push front" << endl;
 	for (int i = 0; i < n;i++)
 	{
 		list.push_front(rand() % 100);
 	}
 	list.print();
-	list.reverse_print();
+	cout << delimeter;
+	cout << "Push back" << endl;
+	list.push_back(rand() % 100);
+	list.print();
+	cout << delimeter;
+	cout << "Pop front" << endl;
+	list.pop_front();
+	list.print();
+	cout << delimeter;
+	cout << "Pop back" << endl;
+	list.pop_back();
+	list.print();
+	cout << delimeter;
+	cout << "Insert" << endl;
+	int insertIndex;
+	int insertData;
+	cout << "Введите что вставить: "; cin >> insertData;
+	cout << "Введите индекс куда вставить: "; cin >> insertIndex;
+	list.insert(insertData, insertIndex);
+	list.print();
+	cout << delimeter;
+	cout << "Erase" << endl;
+	int eraseIndex;
+	cout << "Индекс элемента для удаления: "; cin >> eraseIndex;
+	list.erase(eraseIndex);
+	list.print();
+	cout << delimeter;
+	//list.reverse_print();
 
 
 }
